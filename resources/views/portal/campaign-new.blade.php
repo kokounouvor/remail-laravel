194958dd9d276d5c89e70d26a20fa6039d79bbbd
kokounouvor <kokounouvor@gmail.com>
2025-04-22 08:30:34 +0000

    Campaign publisher email service listing update

--- resources/views/portal/campaign-new.blade.php
+++ resources/views/portal/campaign-new.blade.php
@@ -91,7 +91,7 @@
                                     @else
                                     <select type="text" name="from_email" class="py-2 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="">
                                         @foreach($emails as $ls)
-                                        <option value="{{$ls->mail_username}}"> {{$ls->mail_username}} </option>
+                                        <option value="{{$ls->mail_username}}"> {{$ls->name}} </option>
                                         @endforeach
                                     </select>
                                     <p class="text-xs italic mt-1 text-red-600">A selectionner parmis vos serveurs Mail préenregistré</p>
